5bfcffdf0206546375c13e071e30f9d5386a90b1
91989 <amolsalve63@gmail.com>
2022-05-22 21:49:27 +0530

    gggg

---
+++ one.py
@@ -0,0 +1,16 @@
+#a = int(input("Enter a number: "))
+#square = a**0.5
+#print("the square root is " , square)
+
+#b = int(input("enter a number:"))
+#c = int(input("enter a number"))
+#print("answer is", b+c )
+
+#b = int(input("enter a number:"))
+#c = int(input("enter a number"))
+#avd = (b+c)/2
+#print(avd)
+
+word = "amazon"
+d=word[1:2:1]
+print(d)
---
+++ praticestest1.py
@@ -0,0 +1,12 @@
+f = open("C://bigdata//python_project_datasets//block.txt", 'r')
+dt = f.read()
+print(dt)
+
+with open('D://bigdata//PycharmProjects//pythonpratices//block_new1.txt','w') as f:
+    f.write(dt)
+
+f = open("C://bigdata//python_project_datasets//showlay.txt", 'r')
+dt = f.read()
+print(f.read.counts('c'))
+
+
